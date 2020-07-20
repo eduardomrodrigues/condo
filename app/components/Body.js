7eb97ffc0a23e3ee999d6bc51930129cddbbed3b
Eduardo de Moura Rodrigues <eduardom.rodrigues@gmail.com.br>
2020-07-19 21:24:57 -0300

    agora com hooks

--- app/components/Body.js
+++ app/components/Body.js
@@ -1,17 +1,11 @@
 import React from 'react'
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
 import Card from './Card'
 import VoteCard from './VoteCard'
 import PartyCard from './PartyCard'
 import GymCard from './GymCard'
 import BarbecueCard from './BarbecueCard'
-import NewUser from './NewUser'
+import CompleteUser from './CompleteUser'
 
 class Body extends React.Component {
 
@@ -23,27 +17,26 @@ class Body extends React.Component {
     render() {
         return (
             <div className="corpo">
+
+                <Card titulo="Complete seu cadastro" imagem="app/images/complete.png" >
+                    <CompleteUser />
+                </Card>
+
                 <Card titulo="Votação em andamento" imagem="app/images/vote.png" >
                     <VoteCard />
                 </Card>
 
 
                 <Card titulo="Reserva de salão de festas" imagem="app/images/party.png">
-                    <React.Fragment>
-                        <PartyCard />
-                    </React.Fragment>
+                    <PartyCard />
                 </Card>
 
                 <Card titulo="Reserva academia" imagem="app/images/gym.png">
-                    <React.Fragment>
-                        <GymCard />
-                    </React.Fragment>
+                    <GymCard />
                 </Card>
 
                 <Card titulo="Reserva churrasqueira" imagem="app/images/barbecue.png">
-                    <React.Fragment>
-                        <BarbecueCard />
-                    </React.Fragment>
+                    <BarbecueCard />
                 </Card>
             </div>
 
