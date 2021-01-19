d66ec9cdeae1e8e69bf90b8674c00fe7abd47df1
terminal 1 <eduardom.rodrigues@gmail.com.br>
2021-01-19 17:19:35 -0300

    delete tudo

--- src/events.js
+++
@@ -1,9 +0,0 @@
-export default [
-    {
-      'title': 'Meu churras',
-      'bgColor': '#ff7f50',
-      'allDay': true,
-      'start': new Date(2020, 6, 9),
-      'end': new Date(2020, 6, 9)
-    }
-  ]
